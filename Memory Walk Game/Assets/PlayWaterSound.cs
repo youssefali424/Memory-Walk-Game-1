aa967b18a67131e31614537c263686c24470e026
Mohamed Ramadan <mohamed_ramadan154@hotmail.com>
2018-05-11 17:42:22 +0200

    Handle Hydrants Collision
    Add Water Flow and its sound effect, and play it on hydrant break.

---
+++ Memory Walk Game/Assets/PlayWaterSound.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayWaterSound : MonoBehaviour {
+
+    private ParticleSystem ps;
+    private AudioSource effect;
+	// Use this for initialization
+	void Start () {
+        ps = gameObject.GetComponent<ParticleSystem>();
+        effect = gameObject.GetComponent<AudioSource>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (ps.isPlaying && !effect.isPlaying)
+        {
+            effect.Play();
+        }
+        
+	}
+}
